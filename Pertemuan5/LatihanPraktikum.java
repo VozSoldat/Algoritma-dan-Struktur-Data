2751e67d8e9ea074c23853e65931f80be13277f4
Muhammad Erril <133947567+VozSoldat@users.noreply.github.com>
2024-03-24 23:38:01 +0700

    update

--- Pertemuan5/LatihanPraktikum.java
+++ Pertemuan5/LatihanPraktikum.java
@@ -11,24 +11,27 @@ public class LatihanPraktikum {
         double[] accel = {6816, 3921, 4360, 4058, 6255, 3700, 4180, 4180};
         double[] power = {728,   575, 657,  609,  703,  553,  609,  631};
 
+        System.out.println("======================");
         System.out.println("max top acceleration");
-        System.out.println(topAccelDC(accel, 0, (accel.length-1)));
+        System.out.println(merk[topAccelDC(accel, 0, (accel.length-1))]+", "+ model[topAccelDC(accel, 0, (accel.length-1))]+", "+ accel[topAccelDC(accel, 0, (accel.length-1))]);
 
+        System.out.println("=======================");
         System.out.println("min top acceleration");
-        System.out.println(minAccelDC(accel, 0, (accel.length-1)));
+        System.out.println(merk[minAccelDC(accel, 0, (accel.length-1))]+", "+ model[minAccelDC(accel, 0, (accel.length-1))]+", "+ accel[minAccelDC(accel, 0, (accel.length-1))]);
 
+        System.out.println("=========================");
         System.out.println("rata-rata power");
         System.out.println(meanTopPower(power));
     }
     
-    static double topAccelDC(double arr[], int l, int r){
+    static int topAccelDC(double arr[], int l, int r){
         if (l == r) {
-            return arr[l];
+            return l;
         } else {
             int mid = (l + r) / 2;
-            double lmax = topAccelDC(arr, l, mid);
-            double rmax = topAccelDC(arr, mid + 1, r);
-            if (lmax > rmax) {
+            int lmax = topAccelDC(arr, l, mid);
+            int rmax = topAccelDC(arr, mid + 1, r);
+            if (arr[lmax] > arr[rmax]) {
                 return lmax;
             } else {
                 return rmax;
@@ -36,14 +39,14 @@ public class LatihanPraktikum {
         }
     }
 
-    static double minAccelDC(double arr[], int l, int r){
+    static int minAccelDC(double arr[], int l, int r){
         if (l == r) {
-            return arr[l];
+            return l;
         } else {
             int mid = (l + r) / 2;
-            double lmin = minAccelDC(arr, l, mid);
-            double rmin = minAccelDC(arr, mid + 1, r);
-            if (lmin < rmin) {
+            int lmin = minAccelDC(arr, l, mid);
+            int rmin = minAccelDC(arr, mid + 1, r);
+            if (arr[lmin] < arr[rmin]) {
                 return lmin;
             } else {
                 return rmin;
