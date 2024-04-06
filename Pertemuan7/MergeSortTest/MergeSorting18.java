47cc56fa6c50bfe6e377d024ec6f13d0107e0e23
Muhammad Erril <133947567+VozSoldat@users.noreply.github.com>
2024-04-06 08:44:21 +0700

    update

--- Pertemuan7/MergeSortTest/MergeSorting18.java
+++ Pertemuan7/MergeSortTest/MergeSorting18.java
@@ -6,38 +6,36 @@ package Pertemuan7.MergeSortTest;
 public class MergeSorting18 {
 
     public void mergeSort(int[] data, int left, int middle, int right){
-        int[] temp = new int[data.length];
+        int[] temp = new int[right - left + 1];
         for (int i = left; i <= right; i++) {
-            temp[i] = data[i];
+            temp[i - left] = data[i];
         }
 
-        int a = left;
-        int b = middle +1;
+        int a = 0;
+        int b = middle - left + 1;
         int c = left;
 
-        while (a <= middle && b <= right) {
+        while (a <= middle - left && b <= right - left) {
             if (temp[a] <= temp[b]) {
                 data[c]= temp[a];
                 a++;
-            }else{
+            } else {
                 data[c] = temp[b];
                 b++;
             }
             c++;
         }
-        int s = middle -a;
+        int s = middle - left - a;
         for (int i = 0; i <= s; i++) {
             data[c+i] = temp[a+i];
         }
-
-        sort(data, left, right);
-        
     }
+    
     public void sort(int data[], int left, int right){
         if (left < right) {
-            int middle = (left + right)/2;
+            int middle = (left + right) / 2;
             sort(data, left, middle);
-            sort(data, middle +1, right);
+            sort(data, middle + 1, right);
             mergeSort(data, left, middle, right);
         }
     }
